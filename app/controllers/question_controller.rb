27bfc48b5148fba25b152626f7dcd1ca5f053df1
Mark Donahue <mdonagh@icloud.com>
2017-04-14 19:29:05 -0700

    plain HTML content

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,5 +1,13 @@
 class QuestionController < ApplicationController
+	
 	def show
-		@question = Question.find(params[:id])
+		@question_id = params[:id]
+	end
+
+	def answer
+		Answer.create(answer_text: params[:answer_text], question_id: params[:question_id], user_id: current_user.id)
+		@show_answer = true
+		@question_id = params[:question_id]
+		render 'show'
 	end
 end
