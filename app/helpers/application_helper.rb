7af954a7d302b3e250aeedaa26b3180097310f56
Mark Donahue <mdonagh@icloud.com>
2017-04-19 20:36:30 -0700

    Re-organized partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def getSectionName(question_id)
+		puts 'hokkaido'
+		puts question_id
+		case question_id.to_i
+		when 1..27
+		  return 'basics'
+		when 28..50
+		  return 'data-structures'
+		else
+		  return "unknown-question-id"
+		end
+	end
 end
